fba873ef80a1f3bde501709e25b22a33b402a1b3
KeerthanaReddy-15 <keerthanareddytholeti@gmail.com>
2025-07-06 22:54:04 +0530

    Fix: corrected _name_ typo

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def redirect_to_original(short_code):
         return redirect(original_url)
     return "Invalid short URL", 404
 
-if _name_ == "_main_":
+if __name__ == ("__main__"):
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
